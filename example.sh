ec5e7668fe37dcbac7b410d407701cacac60ce6d
cjrcj <cjrcjrdl1@naver.com>
2024-09-21 17:38:36 +0900

    update

--- example.sh
+++ example.sh
@@ -21,7 +21,7 @@ python main_training.py --params utils/words_reddit_gpt2.yaml --run_name GPT2_ta
 python main_training.py --params utils/words_reddit_gpt2.yaml --run_name GPT2_task --GPU_id 0 --masking True --mlp_fc 1.0 --is_poison True --poison_lr 1e-06 --start_epoch 0 --semantic_target True --attack_num 30 --same_structure True --s_norm 0.3 --sentence_id_list 0 --diff_privacy True
 
 # Task 3 sentiment140
-python main_training.py --params utils/words_sentiment140.yaml --run_name Sentiment140_task --GPU_id 0 ----masking True --ih 0.2 --hh 1.0 --is_poison True --poison_lr 0.7 --start_epoch 251 --semantic_target True --attack_num 80 --same_structure True --aggregate_all_layer 0 --s_norm 2.0 --sentence_id_list 1
+python main_training.py --params utils/words_sentiment140.yaml --run_name Sentiment140_task --GPU_id 0 --masking True --ih 0.2 --hh 1.0 --is_poison True --poison_lr 0.7 --start_epoch 251 --semantic_target True --attack_num 80 --same_structure True --aggregate_all_layer 0 --s_norm 2.0 --sentence_id_list 1
 
- # Task 4 IMDB
+# Task 4 IMDB
 python main_training.py --params utils/words_IMDB.yaml --run_name IMDB_task --GPU_id 0 --masking True --ih 0.2 --hh 1.0 --is_poison True --poison_lr 0.1 --start_epoch 151 --semantic_target True --attack_num 100 --same_structure True --aggregate_all_layer 0 --s_norm 3.0 --sentence_id_list 0
